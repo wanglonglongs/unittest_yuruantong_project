e01e59968e9d992cae412c8dcc1f495bdef87c54
admin <wanglonglongiii@163.com>
2023-04-17 14:37:08 +0800

    移动文件路径

--- yuruantong_pc/main.py
+++ yuruantong_pc/main.py
@@ -2,6 +2,5 @@ import os
 import unittest
 
 if __name__ == "__main__":
-    suite = unittest.defaultTestLoader.discover(os.path.join(os.path.dirname(__file__), "tests"), \
-                                                pattern='*.py', top_level_dir=os.path.dirname(__file__))
+    suite = unittest.defaultTestLoader.discover(os.path.join(os.path.dirname(__file__), "tests"),pattern='*.py', top_level_dir=os.path.dirname(__file__))
     unittest.TextTestRunner(verbosity=2).run(suite)
