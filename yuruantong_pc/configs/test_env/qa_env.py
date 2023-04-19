6a87bca07e5f62857e681e6efc5a2d907f150f98
admin <wanglonglongiii@163.com>
2023-04-19 15:32:12 +0800

    整合log日志的功能+

--- yuruantong_pc/configs/test_env/qa_env.py
+++
@@ -1,2 +0,0 @@
-DOMAIN = "http://test.yuruantong.com/amp/?wholeManagement"
-Account = {"username": "18196627126", "password": "aaaa123456"}
---
+++ yuruantong_pc/result/log/Log_nb.py
@@ -0,0 +1,11 @@
+from nb_log import get_logger
+
+logger = get_logger('登记房东')   # get_logger 只有一个name是必传递的，其他的参数不是必传。
+# logger = get_logger('lalala',log_filename='lalala.log',formatter_template=5,log_file_handler_type=2) # get_logger有很多其他入参可以自由定制logger。
+
+
+logger.debug(f'debug是绿色，说明是调试的，代码ok ')
+logger.info('info是天蓝色，日志正常 ')
+logger.warning('黄色yello，有警告了 ')
+logger.error('粉红色说明代码有错误 ')
+logger.critical('血红色，说明发生了严重错误 ')
