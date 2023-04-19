43f41c47d14ea03e633af02fa95186164afa4b08
lixun <1367446518@qq.com>
2023-04-20 02:14:36 +0800

    登记租客 入参-随机输入

--- yuruantong_pc/tests/test_registered_tenant_case.py
+++ yuruantong_pc/tests/test_registered_tenant_case.py
@@ -10,9 +10,10 @@ from yuruantong_pc.common.packaging_methon.yu_ruan_login import LoginPage
 import random
 from yuruantong_pc.common.error_screenshot import Screen
 import string
+import datetime
+
 
 class registeredTenantCase(unittest.TestCase):
-
     driver = webdriver.Chrome()
 
     @classmethod
@@ -52,12 +53,9 @@ class registeredTenantCase(unittest.TestCase):
 
         time.sleep(3)
 
-
     # 租赁状态 未租选择
     @Screen(driver=driver)
     def test_zuLinStatus(self):
-
-
         # 租赁状态下拉
         lease_status = self.element_whole["LEASE_STATUS"]
         self.driver.execute_script(lease_status)
@@ -76,39 +74,87 @@ class registeredTenantCase(unittest.TestCase):
         self.driver.execute_script(check_tenant)
         time.sleep(1)
 
-        # 生成随机字符
-        def random_string_generator():
-            print(string.ascii_letters)
-            print(string.punctuation)
-            return ''.join(random.sample(string.ascii_letters, 8))
+        # 随机生成 某区间 内的任意一个整数
+        def free_random_one_num(start_num, end_num):
+            return random.randint(start_num, end_num)
+
+        # 随机生成X位字符 纯数字
+        def free_random_many_num(num_sign):
+            return "".join(map(lambda x:random.choice(string.digits), range(num_sign)))
+
+        # 随机生成X位字符 纯字母
+        def random_string_generator(num_sign):
+            random_char = random.sample(string.ascii_letters, num_sign)
+            return ''.join(random_char)
+            # now_date = str(datetime.date.today())
+            # return ''.join(random_char) + now_date
+
+        # 随机生成X位字符 字母+数字组合
+        def random_string_number(n):
+            m = random.randint(1, n)
+            a = "".join([str(random.randint(0, 9)) for _ in range(m)])
+            b = "".join([random.choice(string.ascii_letters) for _ in range(n - m)])
+            return ''.join(random.sample(list(a + b), n))
+
+        print(string.digits+string.ascii_letters+string.punctuation)
+
+        # 随机生成手机号 暂只对第二位做要求
+        def random_create_phone():
+            # 第二位数字
+            second = [3, 4, 5, 6, 7, 8][random.randint(0, 4)]
+            # 最后九位数字
+            suffix = random.randint(99999999, 1000000000)
+            # 拼接手机号
+            return "1{}{}".format(second, suffix)
+        print(random_create_phone())
 
         # 租客姓名
-        # tenant_name = self.element_whole["TENANT_NAME"].value = random_string_generator()
-        # print(tenant_name)
-        # tenant_name = self.element_whole["TENANT_NAME"].value = {random_string_generator()}
-        # tenant_name = self.element_whole["TENANT_NAME"] = random_string_generator()
-
-        # tenant_name = self.element_whole["TENANT_NAME"] = “123abc”
-
-        # 要加f , js抽出 无法塞值
-        tenant_name = f'document.querySelector("#mainDiv > div > div.hasTagsView.main-container > section > div > wujie-app").shadowRoot.querySelector("#app > div > div:nth-child(2) > div > div.registerTenlentStyle > div > div.container > div.formStyle > form > div:nth-child(4) > div:nth-child(1) > div > div > div > div > input").value = "{random_string_generator()}" '
-        print(tenant_name)
+        tenant_name = f'document.querySelector("#mainDiv > div > div.hasTagsView.main-container > section > div > ' \
+                      f'wujie-app").shadowRoot.querySelector("#app > div > ' \
+                      f'div:nth-child(2) > div > div.registerTenlentStyle > div > div.container > div.formStyle > ' \
+                      f'form > div:nth-child(4) > div:nth-child(1) > div > ' \
+                      f'div > div > div > input").value = "{random_string_number(free_random_one_num(8,10))}" '
         self.driver.execute_script(tenant_name)
 
+        # 证件选择（要先点开下拉框 再去选择）
+        identity_card_input = f'document.querySelector("#mainDiv > div > div.hasTagsView.main-container > section > ' \
+                               f'div > wujie-app").shadowRoot.querySelector("#app > div > div:nth-child(2) > div > ' \
+                               f'div.registerTenlentStyle > div > div.container > div.formStyle > form > ' \
+                               f'div:nth-child(4) > div:nth-child(2) > div > div.el-select.el-select--medium > div > ' \
+                               f'input").click()'
+        self.driver.execute_script(identity_card_input)
 
+        # 默认台胞证
+        identity_card_choose = f'document.querySelector("#mainDiv > div > div.hasTagsView.main-container > section > ' \
+                                f'div > wujie-app").shadowRoot.querySelector("body > div.el-select-dropdown.el-popper ' \
+                                f'> div.el-scrollbar > div.el-select-dropdown__wrap.el-scrollbar__wrap > ul > ' \
+                                f'li:nth-child({free_random_one_num(3,3)}) > span").click()'
+        self.driver.execute_script(identity_card_choose)
 
-    # 登记租客信息填写
-    # @Screen(driver=driver)
-    # def test_registered_tenant_info(self):
-        # print('进来了1')
-        # # 点击登记租客
-        # check_tenant = 'document.querySelector("#mainDiv > div > div.hasTagsView.main-container > section > div > wujie-app").shadowRoot.querySelector("#app > div > div:nth-child(2) > div > div:nth-child(1) > section > main > div.el-card.table-container.is-never-shadow > div > div > div > div.el-table__fixed-right > div.el-table__fixed-body-wrapper > table > tbody > tr:nth-child(1) > td.el-table_1_column_24.is-center.el-table__cell > div > div:nth-child(1) > button").click()'
-        # print('进来了2')
-        # self.driver.execute_script(check_tenant)
+        # 证件号码 台胞证>8位
+        identity_card = f'document.querySelector("#mainDiv > div > div.hasTagsView.main-container > section > div > ' \
+                        f'wujie-app").shadowRoot.querySelector("#app > div > ' \
+                        f'div:nth-child(2) > div > div.registerTenlentStyle > div > div.container > div.formStyle > ' \
+                        f'form > div:nth-child(4) > div:nth-child(2) > div > ' \
+                        f'div.el-input.el-input--medium > input").value = "{random_string_generator(free_random_one_num(8,11))}"'
+        self.driver.execute_script(identity_card)
 
+        # 联系电话
+        phone = f'document.querySelector("#mainDiv > div > div.hasTagsView.main-container > section > div > ' \
+                       f'wujie-app").shadowRoot.querySelector("#app > div > div:nth-child(2) > div > ' \
+                       f'div.registerTenlentStyle > div > div.container > div.formStyle > form > div:nth-child(4) > ' \
+                       f'div:nth-child(3) > div > div > div > div > input").value = "{random_create_phone()}"'
+        self.driver.execute_script(phone)
+
+        # 紧急联系电话
+        urgency_phone = f'document.querySelector("#mainDiv > div > div.hasTagsView.main-container > section > div > ' \
+                        f'wujie-app").shadowRoot.querySelector("#app > div > div:nth-child(2) > div > ' \
+                        f'div.registerTenlentStyle > div > div.container > div.formStyle > form > div:nth-child(4) > ' \
+                        f'div:nth-child(4) > div > div > div > div > input").value = "{random_create_phone()}"'
+        self.driver.execute_script(urgency_phone)
 
     @classmethod
     def tearDownClass(cls):
-        time.sleep(2)
+        time.sleep(5)
         # 关闭浏览器对象
         cls.driver.quit()
