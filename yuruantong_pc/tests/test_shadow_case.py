f52f1aad7c3b0b92124ca5c6dd75c7af4ed5893c
admin <wanglonglongiii@163.com>
2023-04-26 16:03:41 +0800

    修改生成的日志文件

---
+++ yuruantong_pc/tests/test_shadow_case.py
@@ -0,0 +1,70 @@
+# coding:utf-8
+import time
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from yuruantong_pc.common.error_screenshot import Screen
+from yuruantong_pc.common.yaml_helper import YamlHelper
+from yuruantong_pc.common.packaging_methon.yu_ruan_login import LoginPage
+from nb_log import LogManager
+from pyshadow.main import Shadow
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+class wholeManagementCase(unittest.TestCase):
+
+    driver = webdriver.Chrome()
+
+    @classmethod
+    def setUpClass(cls):
+        # 创建Chrome浏览器对象
+        cls.driver.maximize_window()
+        cls.element_locator_yaml = '../configs/element_locator/fang_dong_login.yaml '
+        cls.element = YamlHelper.read_yaml(cls.element_locator_yaml)
+        cls.wait = WebDriverWait(cls.driver, 10, poll_frequency=0.5)
+        cls.logger = LogManager('登记房东').get_logger_and_add_handlers(10,log_filename='登记房东.log')
+        cls.shadow = Shadow(cls.driver)
+
+    @Screen(driver=driver)
+    def test_login_yuRuanTong(self):
+        # 创建LoginPage对象
+        login_page = LoginPage(self.driver)
+
+        # 调用login()方法
+        login_page.login("18196627126","aaaa123456")
+        # logger.info('登录成功')
+        self.logger.info("登录寓软通账号成功")
+
+    @Screen(driver=driver)
+    def test_openTag_page(self):
+        # 点击菜单选项栏-房源
+        self.wait.until(EC.presence_of_element_located(
+            (By.XPATH, self.element["MENU_HOUSING_RESOURCES"]))).click()
+        time.sleep(2)
+
+        # 点击二级菜单按钮-整租管理
+        self.wait.until(EC.presence_of_element_located(
+            (By.XPATH, self.element["MENU_WHOLE_MANAGEMENT"]))).click()
+
+        # 验证当前已经打开整租页标签
+        jump_housing_results = self.wait.until(
+            EC.presence_of_element_located((By.XPATH, self.element["TAG_NAME_WHOLE_MANAGEMENT"]))).get_attribute('text')
+        self.assertEqual("整租管理" in jump_housing_results, True)
+        self.logger.info("打开整租管理页面成功")
+
+    def test_pen_register_landlord(self):
+        # parent_element = self.driver.find_element(By.XPATH, '//wujie-app')
+        # 访问 shadowRoot 属性
+        # shadow_root = self.driver.execute_script('return arguments[0].shadowRoot', parent_element)
+        time.sleep(5)
+        ActionChains(self.driver).move_by_offset(1749,100).click().perform()
+        time.sleep(2)
+        ActionChains(self.driver).move_by_offset(450,250).click().perform()
+
+    @classmethod
+    def tearDownClass(cls):
+        time.sleep(5)
+        # 关闭浏览器对象
+        cls.driver.quit()
